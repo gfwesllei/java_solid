f41194c65638cbb55303cd7021702adc03237a40
wesllei.ferreira <wesllei.gustavo@gmail.com>
2021-05-13 11:32:49 -0300

    Refacator class Employe.java, add variable salary

--- src/main/java/com/tdd/pratices/_1srp/violation/Employe.java
+++ src/main/java/com/tdd/pratices/_1srp/violation/Employe.java
@@ -10,14 +10,15 @@ public class Employe {
     private String name;
     private String cpf;
     private Role role;
+    double salary;
+
 
     public double calculateWage(){
-        double baseWage=4000;
         if (Role.DEVELOPEMENT.equals(getRole())){
-            return baseWage + baseWage*0.3;
+            return salary + salary*0.3;
         }
         if (Role.PEOPLE_SUPORTE.equals(getRole())){
-            return baseWage + baseWage*0.1;
+            return salary + salary*0.1;
         }
         throw  new WageCalculateExcption("Canot calcule wage");
     }
