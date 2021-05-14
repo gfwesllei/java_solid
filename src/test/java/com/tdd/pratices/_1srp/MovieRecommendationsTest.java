688ef6b73c08561d2aa67e0361b9d3ef20019c48
wesllei.ferreira <wesllei.gustavo@gmail.com>
2021-05-14 17:57:13 -0300

    Optimize imports from MovieRecommendationsTest

--- src/test/java/com/tdd/pratices/_1srp/MovieRecommendationsTest.java
+++ src/test/java/com/tdd/pratices/_1srp/MovieRecommendationsTest.java
@@ -12,7 +12,6 @@ import org.mockito.MockitoAnnotations;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
